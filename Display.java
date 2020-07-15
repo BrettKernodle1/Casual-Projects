4add6dec25f1e1421950ddf83e4f69efa71c9805
Brett Kernodle <60923672+BrettKernodle1@users.noreply.github.com>
2020-07-15 12:36:09 -0500

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Display extends JFrame {
+
+    private Canvas canvas;
+
+    public Display(int width, int height){
+        setTitle("The Game NO.2");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setFocusable(false);
+        add(canvas);
+        pack();
+
+        canvas.createBufferStrategy(3 );
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void render(Game game){
+        BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        Rectangle rectangle = game.getRectangle();
+        graphics.setColor(Color.BLUE);
+        graphics.fillRect(
+                (int) rectangle.getX(),
+                (int) rectangle.getY(),
+                (int) rectangle.getWidth(),
+                (int) rectangle.getHeight()
+        );
+
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+
+
+}
